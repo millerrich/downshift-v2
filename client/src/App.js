cd6162f2cea4abf87fb9d949f20e19e90b0fe810
Miller Rich <william.miller.rich@gmail.com>
2020-10-26 20:55:49 -0500

    update home page, set state of timekeeper to current time using moment on load, change user welcome message in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Signup from './components/sign-up'
 import LoginForm from './components/login-form'
 import Navbar from './components/navbar'
 import Home from './components/home'
+import Set from './pages/set';
 
 class App extends Component {
   constructor() {
@@ -56,12 +57,12 @@ class App extends Component {
         <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
         {/* greet user if logged in: */}
         {this.state.loggedIn &&
-          <p>Join the party, {this.state.username}!</p>
+          <p>Hello, {this.state.username}, welcome to Downshift!</p>
         }
         {/* Routes to different components */}
         <Route
           exact path="/"
-          component={Home} />
+          component={Set} />
         <Route
           path="/login"
           render={() =>
