d6103b7f382694f9926ba4e1fb8c00cfaa56d167
Miller Rich <william.miller.rich@gmail.com>
2020-10-30 14:06:18 -0500

    update process.env

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 app.use(
   session({
     secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
-    store: new MongoStore({ mongooseConnection: dbConnection }),
+    // store: new MongoStore({ mongooseConnection: dbConnection }),
     resave: false, //required
     saveUninitialized: false //required
   })
