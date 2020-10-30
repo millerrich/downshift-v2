7664598b80d0a93ac3d64e5977c696ff2f08009d
Henry Parrish <henryhparrish@gmail.com>
2020-10-30 10:56:05 -0500

    Added login/signup styling and new structure

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import axios from 'axios'
+import { Button, Card, Form, Container, Row, Col, CardGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class LoginForm extends Component {
   constructor() {
@@ -52,48 +54,65 @@ class LoginForm extends Component {
       return <Redirect to={{ pathname: this.state.redirectTo }} />
     } else {
       return (
-        <div>
-          <h4>Login</h4>
-          <form className="form-horizontal">
-            <div className="form-group">
-              <div className="col-1 col-ml-auto">
-                <label className="form-label" htmlFor="username">Username</label>
-              </div>
-              <div className="col-3 col-mr-auto">
-                <input className="form-input"
-                  type="text"
-                  id="username"
-                  name="username"
-                  placeholder="Username"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-1 col-ml-auto">
-                <label className="form-label" htmlFor="password">Password: </label>
-              </div>
-              <div className="col-3 col-mr-auto">
-                <input className="form-input"
-                  placeholder="password"
-                  type="password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div className="form-group ">
-              <div className="col-7"></div>
-              <button
-                className="btn btn-primary col-1 col-mr-auto"
+        <>
 
-                onClick={this.handleSubmit}
-                type="submit">Login</button>
-            </div>
-          </form>
-        </div>
+          <Container className="formMargin">
+            <CardGroup className="cG" style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}>
+              <Col lg={6} sm={12}>
+                <Card id="port" border="dark" className="text-center mt-5 bS" style={{ width: '20rem', height: '15rem', }}>
+                  <Card.Body>
+                    <Card.Title>
+                     <h2 className="loginTitle">Welcome Back!</h2> 
+                    </Card.Title>
+                    <Card.Text>
+                      If you do not have an account <br />click the sign up button below <br />
+                    </Card.Text>
+
+                    <Card.Text>
+                      <Link to="/signup" className="signupSwitch">
+                        <span className="text-secondary signColor">Sign Up</span>
+                      </Link>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col lg={6} sm={12}>
+                <Card id="port"  className="text-center mt-5 bS1" style={{ width: '20rem', height: '15rem' }}>
+                  <Card.Body>
+                  <Card.Title>
+                    <h4 style={{fontWeight:'Bold'}}>Login</h4>
+                  </Card.Title>
+                    <Card.Text>
+                      <input className="form-input"
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                      />
+                      <input className="form-input"
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                      />
+                    </Card.Text>
+                    <Card.Text className="displaySm" style={{ display: 'flex', justifyContent: 'center', }}>
+                      <Button  className="but" onClick={this.handleSubmit}
+                        type="submit">Login</Button>
+                       
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </CardGroup>
+          </Container>
+        </>
       )
     }
   }
